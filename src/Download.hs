8b49f709341aed0ca468c52da4adf1ddb682fac3
Mario Apra <mario_surfing@hotmail.com>
2017-10-06 22:52:31 +1300

    Remove unecessary imports on Download.hs

--- src/Download.hs
+++ src/Download.hs
@@ -2,16 +2,11 @@
 
 module Download where
 
-import Control.Applicative ((<$>), optional)
-import Data.Maybe (fromMaybe)
-import Data.Text (Text)
 import Data.Text.Lazy (unpack)
 import Happstack.Lite
-import Text.Blaze.Html5 (Html, (!), a, form, input, p, toHtml, label)
-import Text.Blaze.Html5.Attributes (action, enctype, href, name, size, type_, value)
-import qualified Text.Blaze.Html5 as H
+import Text.Blaze.Html5 (Html, (!), form, input, label)
+import Text.Blaze.Html5.Attributes (action, enctype, name, type_, value)
 import qualified Text.Blaze.Html5.Attributes as A
-import System.Directory (renameFile)
 
 import Template
 
