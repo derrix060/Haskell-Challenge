931c03c7a69777d25bd3ed87a7474616a4bfd7e8
Mario Apra <mario_surfing@hotmail.com>
2017-10-06 16:36:20 +1300

    update fileServing to form and download

--- src/Main.hs
+++ src/Main.hs
@@ -106,8 +106,25 @@ fortune = msum [ viewFortune, updateFortune ]
 
 
 fileServing :: ServerPart Response
-fileServing =
-    serveDirectory EnableBrowsing ["index.html"] "."
+fileServing = msum [ downloadForm, handleDownload ]
+  where
+    -- Form to download one file if GET request
+    downloadForm :: ServerPart Response
+    downloadForm =
+        do method GET
+           ok $ template "Download Form" $
+            form ! action "/download" ! enctype "multipart/form-data" ! A.method "POST" $ do
+              label ! A.for "file" $ "Enter the file name: "
+              input ! type_ "text" ! A.id "file" ! name "file"
+              input ! type_ "submit" ! value "Download it!"
+
+    -- Download the file if POST request
+    handleDownload :: ServerPart Response
+    handleDownload =
+        do method POST
+           mFile <- lookText "file"
+           serveDirectory DisableBrowsing ["index.html"] ("./files/" ++ unpack mFile)
+
 
 
 upload :: ServerPart Response
